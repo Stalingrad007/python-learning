574cd8dcc126dd732b0d935f116a02f268ed1cbf
StalinGrad007 <krogemer@gmail.com>
2026-08-10 04:41:52 +0100

    Day 1: Python basics

---
+++ day01_basics.py
@@ -0,0 +1,34 @@
+my_name = "john doe"
+age = 99
+height = 1.98
+fav_food = "pizza"
+is_learn_python = True
+
+print(f"my name is {my_name}")
+print(f"my age is {age} Yo")
+print(f"my height is {height}")
+print(f"my fav food {fav_food}")
+if is_learn_python:
+    print("good job")
+else:
+    print("def not a good job")
+
+price = 15
+quantity = 3
+total = price * quantity
+
+print(f"total price is {total}")
+
+if age >= 18:
+    print(" you are a adult")
+else:
+    print("you are a minor")
+
+name = input("what is your name?: ")
+print(f"hello {name}!")
+the_age = int(input("how old are you?: "))
+print(f"you are {the_age} years old.")
+ten_years  = the_age + 10
+print(f"in ten years you gonna turn {ten_years}")
+the_fav_food = input("what is your favorite food?: ")
+print(f"Your favorite food is {the_fav_food}")
